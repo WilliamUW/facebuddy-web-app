aaafe5d253afdb4e7cefacd9afd7bdead0949976
William Wang <bwilliamwang@gmail.com>
2025-02-26 14:52:56 -0700

    Revert "clean up code so i can understand it"
    This reverts commit 0e5462a4c2924956660b662ba624a3ee83a33428.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,40 @@
-import "./global.css";
-import "@coinbase/onchainkit/styles.css";
-import "@rainbow-me/rainbowkit/styles.css";
+import './global.css';
+import '@coinbase/onchainkit/styles.css';
+import '@rainbow-me/rainbowkit/styles.css';
 
-import type { Metadata } from "next";
-import { NEXT_PUBLIC_URL } from "../config";
-import dynamic from "next/dynamic";
-import Header from "src/components/Header";
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from '../config';
+import dynamic from 'next/dynamic';
+
 const OnchainProviders = dynamic(
-  () => import("src/components/OnchainProviders"),
+  () => import('src/components/OnchainProviders'),
   {
     ssr: false,
-  }
+  },
 );
 
 export const viewport = {
-  width: "device-width",
+  width: 'device-width',
   initialScale: 1.0,
 };
 
 export const metadata: Metadata = {
-  title: "FaceBuddy",
-  description: "Connect and Pay with 1 Click",
+  title: 'FaceBuddy',
+  description: 'Connect and Pay with 1 Click',
   openGraph: {
-    title: "FaceBuddy",
-    description: "Connect and Pay with 1 Click",
+    title: 'FaceBuddy',
+    description: 'Connect and Pay with 1 Click',
     images: [`${NEXT_PUBLIC_URL}/vibes/vibes-19.png`],
   },
 };
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="flex items-center justify-center">
-        <OnchainProviders>
-          <div>
-            <Header />
-            {children}
-          </div>
-        </OnchainProviders>
+        <OnchainProviders>{children}</OnchainProviders>
       </body>
     </html>
   );
